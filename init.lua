be22c6654764662c320d3b2cf38452dded54f614
Rick Ryan <rick.ryan2@outlook.com>
2023-11-02 00:50:43 +0000

    chore: setup the color the color schema

--- init.lua
+++ init.lua
@@ -154,9 +154,13 @@ require('lazy').setup({
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
-    config = function()
+    config = function(_, opts)
+      require('onedark').setup(opts)
       vim.cmd.colorscheme 'onedark'
     end,
+    opts = {
+      style = 'cool'
+    },
   },
 
   {
