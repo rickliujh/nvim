a46b96c9dd80663c82769b773b41baef55a22917
Rick Ryan <rick.ryan2@outlook.com>
2023-11-05 19:11:16 +0000

    feat: adding useful remap for vim with comment explaining

--- init.lua
+++ init.lua
@@ -634,8 +634,43 @@ cmp.setup {
 -- vim: ts=2 sts=2 sw=2 et
 --
 --[[VIM Re-mapping]]
+
+-- moveing things selected around in v mode
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
-vim.keymap.set('n' ,'<C-d>', '<C-d>zz')
-vim.keymap.set('n' ,'<C-u>', '<C-u>zz')
+-- append next line to this line recursively 
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- keep cursor in the center when you moving around
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste but still keeping the it
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- yank to '+' register (system)
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- delete without add it to clipboard aka black register, register '_'
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- aviod using quikcsearch
+vim.keymap.set("n", "Q", "<nop>")
+
+-- using tmux to quick switch between projects
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replace the word in current cursor
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- make current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
